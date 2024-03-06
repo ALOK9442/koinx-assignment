d1ec3110b6e8aab1e111430ffcddfee36ca2df7c
ALOK9442 <alok2110036@akgec.ac.in>
2024-03-06 20:46:58 +0530

    removed context wrapper api

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,12 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <CoinContextProvider>
         <Navbar></Navbar>
         <div className=" w-[full] h-[full] flex items-center justify-center bg-[#eff2f5] overflow-x-hidden max-[411px]:px-2">
         {children}
         </div>
-        </CoinContextProvider>
         </body>
     </html>
   );
